89d3f18dbac3abc9b2123b8acdd0c53d641968bd
jwdev66 <jwdev66@gmail.com>
2021-03-11 04:12:38 +0500

    Step12:- Update Blogs page based on content amount and add page title
    1:- Updated
    	a:- src/pages/blogs.tsx

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "../components/addOns/Layout";
+import { graphql, useStaticQuery } from "gatsby";
 import { BlogsContent } from "../components/blogs/BlogsContent";
 import { BlogPageEmpty } from "../components/blogs/BlogPageEmpty";
-import Layout from "../components/addOns/Layout";
 
 export default function Blogs() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulBlogPost {
+          totalCount
+        }
+      }
+    `
+  );
   return (
     <div>
-      <Layout>
-        <BlogsContent />
-        <BlogPageEmpty />
-      </Layout>
+      <Helmet>
+        <title>Blogistan | Blogs</title>
+      </Helmet>
+      <div>
+        <Layout>
+          {data.allContentfulBlogPost.totalCount === 0 ? (
+            <BlogPageEmpty />
+          ) : (
+            <BlogsContent />
+          )}
+        </Layout>
+      </div>
     </div>
   );
 }
