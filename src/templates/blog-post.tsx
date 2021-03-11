47ef46322162ce71cdb1010427d529ba598f19a9
jwdev66 <jwdev66@gmail.com>
2021-03-12 03:01:43 +0500

    15:- Enable Netlify identity in app
    a:- Installed
    	a:- Netlify identity widget
    		i:- "yarn add netlify-identity-widget"
    b:- Update
    	a:- src\templates\blog-post.tsx
    	b:- src\templates\blog-post.css

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./blogPost.css";
 import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 import Layout from "../components/addOns/Layout";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { Button } from "@material-ui/core";
+import netlifyIdentity from "netlify-identity-widget";
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+      summary
+      free
       body {
         raw
       }
@@ -28,6 +32,21 @@ export const query = graphql`
 `;
 
 const BlogPost = (props: any) => {
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  netlifyIdentity.on(
+    "login",
+    (user: { user_metadata: { full_name: React.SetStateAction<string> } }) => {
+      netlifyIdentity.close();
+      setUser(user.user_metadata.full_name);
+    }
+  );
+  netlifyIdentity.on("logout", () => {
+    netlifyIdentity.close();
+    setUser("");
+  });
   return (
     <div>
       <Helmet>
@@ -45,11 +64,55 @@ const BlogPost = (props: any) => {
               className='image'
             />
           </div>
-          <div className='content'>
-            {documentToReactComponents(
-              JSON.parse(props.data.contentfulBlogPost.body.raw)
-            )}
-          </div>
+          {props.data.contentfulBlogPost.free ? (
+            <div className='content'>
+              {documentToReactComponents(
+                JSON.parse(props.data.contentfulBlogPost.body.raw)
+              )}
+            </div>
+          ) : (
+            <div>
+              {user === "" ? (
+                <div>
+                  <div className='content'>
+                    {props.data.contentfulBlogPost.summary}
+                  </div>
+                  <div className='content premium'>
+                    This is content is premium. Kindly SignIn to read this
+                    article.
+                  </div>
+                  <div className='buttonDiv'>
+                    <Button
+                      className='signInButton'
+                      onClick={() => {
+                        netlifyIdentity.open();
+                      }}
+                    >
+                      Login
+                    </Button>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className='content'>
+                    {documentToReactComponents(
+                      JSON.parse(props.data.contentfulBlogPost.body.raw)
+                    )}
+                  </div>
+                  <div className='buttonDiv'>
+                    <Button
+                      className='signInButton'
+                      onClick={() => {
+                        netlifyIdentity.open();
+                      }}
+                    >
+                      LogOut
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
           <div className='source'>
             <p>
               by: <strong>{props.data.contentfulBlogPost.source}</strong>
